eaddc5cc9f458d693d5f127eacd8af6a26ba6e42
Nicolás Pérez <71397273+nico-perez@users.noreply.github.com>
2021-01-13 16:58:04 +0100

    first commit

---
+++ src/main/java/dev/el_nico/dam2_psp_p2/Main.java
@@ -0,0 +1,14 @@
+package dev.el_nico.dam2_psp_p2;
+
+/**
+ * Hello world!
+ */
+public final class Main {
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
